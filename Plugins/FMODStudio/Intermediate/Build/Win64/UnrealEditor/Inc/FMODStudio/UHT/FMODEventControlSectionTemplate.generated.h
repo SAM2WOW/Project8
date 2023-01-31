680733dc51e36e2edc18798e9936589506226b98
Sam Feng <fengyuan20000303@gmail.com>
2023-01-30 23:56:13 -0800

    Added FMOD Plugin

---
+++ Plugins/FMODStudio/Intermediate/Build/Win64/UnrealEditor/Inc/FMODStudio/UHT/FMODEventControlSectionTemplate.generated.h
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Sequencer/FMODEventControlSectionTemplate.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FMODSTUDIO_FMODEventControlSectionTemplate_generated_h
+#error "FMODEventControlSectionTemplate.generated.h already included, missing '#pragma once' in FMODEventControlSectionTemplate.h"
+#endif
+#define FMODSTUDIO_FMODEventControlSectionTemplate_generated_h
+
+#define FID_jk_workspace_Build__2_2__Unreal_Win_integrations_UE4_temp_FMODStudio_HostProject_Plugins_FMODStudio_Source_FMODStudio_Private_Sequencer_FMODEventControlSectionTemplate_h_14_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FFMODEventControlSectionTemplate_Statics; \
+	FMODSTUDIO_API static class UScriptStruct* StaticStruct(); \
+	typedef FMovieSceneEvalTemplate Super;
+
+
+template<> FMODSTUDIO_API UScriptStruct* StaticStruct<struct FFMODEventControlSectionTemplate>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_jk_workspace_Build__2_2__Unreal_Win_integrations_UE4_temp_FMODStudio_HostProject_Plugins_FMODStudio_Source_FMODStudio_Private_Sequencer_FMODEventControlSectionTemplate_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
